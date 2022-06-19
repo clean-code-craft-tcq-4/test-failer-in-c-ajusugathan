b87877704506da7893d7c3a867fdb42e74615ba5
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-06-19 22:02:46 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -7,6 +7,7 @@
 int alertFailureCount = 0;
 
 #if (TEST_CODE_ENABLED == STD_ON)
+
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
@@ -14,6 +15,7 @@ int networkAlertStub(float celcius) {
     // stub always succeeds and returns 200
     return 200;
 }
+
 void testAlertFailure(int Failed_countInital,float TemperatureInCelcious,int networkReturnCode)
 {
     if(TemperatureInCelcious>TEST_THRESHOLD_TEMPERATURE)
@@ -27,23 +29,31 @@ void testAlertFailure(int Failed_countInital,float TemperatureInCelcious,int net
         assert(Failed_countInital==alertFailureCount);
     }
 }
+
 #endif
-void alertInCelcius(float farenheit) {
+
+void alertInCelcius(float farenheit) 
+{
     float celcius = (farenheit - 32) * 5 / 9;
+    
 #if (TEST_CODE_ENABLED == STD_ON)
     int InitialFailureCount = alertFailureCount;
     int returnCode = networkAlertStub(celcius);
 #endif
-    if (returnCode != 200) {
+    
+    if (returnCode != 200) 
+    {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+    
 #if (TEST_CODE_ENABLED == STD_ON)
 testAlertFailure(InitialFailureCount,celcius,returnCode);
 #endif
+    
 }
 
 int main() {
