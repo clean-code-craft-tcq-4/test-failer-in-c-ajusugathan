44e8450c295d095ddc7b487b17f97a6a9a9d7b4a
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-06-26 14:40:48 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -2,6 +2,12 @@
 #include <assert.h>
 #include <string.h>
 
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#define MAX_COLOR_PAIR 25
+
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -9,72 +15,69 @@ const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-int maxCombination =(numberOfMajorColors*numberOfMinorColors);
-
-typedef struct 
-{
-    int  pairNumber;
-    char majorColorName[10];
-    char minorColorName[10];
-}colorPair;
-
- colorPair colorPairMap[25];
- colorPair colorCodeReferenceManual[25];
-
-void CopycolorMap_forTest(int pairCount,int pairNumber,const char* majorColor,const char* minorColor)
-{
-    colorPairMap[pairCount].pairNumber = pairNumber;
-    strcpy(colorPairMap[pairCount].majorColorName , majorColor);
-    strcpy(colorPairMap[pairCount].minorColorName , minorColor);
-}
-
-void generateColorCodeReferenceManual(void)
-{
-    int MajorColorCount = 0;
-    int MinorColorCount = 0;
-    int PairNumber = 1;
-
-    for(MajorColorCount = 0; MajorColorCount < numberOfMajorColors; MajorColorCount++)
-    {
-        for(MinorColorCount = 0; MinorColorCount < numberOfMinorColors; MinorColorCount++)
-        {
-            colorCodeReferenceManual[MajorColorCount*numberOfMajorColors+MinorColorCount].pairNumber = PairNumber;
-            strcpy(colorCodeReferenceManual[MajorColorCount*numberOfMajorColors+MinorColorCount].majorColorName , MajorColorNames[MajorColorCount]);
-            strcpy(colorCodeReferenceManual[MajorColorCount*numberOfMajorColors+MinorColorCount].minorColorName , MinorColorNames[MinorColorCount]);
-            PairNumber++;
-        }
-    }
-}
-
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            CopycolorMap_forTest((i * 5 + j),(i * 5 + j),majorColor[i],minorColor[i]);
-        }
-    }
-    return i * j;
-}
-
-void testColorMap()
-{
-    generateColorCodeReferenceManual();
-    for(int colorMapCount=0;colorMapCount<maxCombination;colorMapCount++)
-    {
-        assert(colorCodeReferenceManual[colorMapCount].pairNumber == colorPairMap[colorMapCount].pairNumber);
-        assert(strcmp(colorCodeReferenceManual[colorMapCount].majorColorName,colorPairMap[colorMapCount].majorColorName) == 0);
-        assert(strcmp(colorCodeReferenceManual[colorMapCount].minorColorName,colorPairMap[colorMapCount].minorColorName) == 0);
-    }
-}
-
-int main() {
     
+char ColorCodeReferenceManual[25][100];
+char colorMapInfo[25][100];
+
+int getPairNumber(int majorColorCount,int minorColorCount)
+{
+    return((majorColorCount * numberOfMajorColors + minorColorCount) + 1);
+}
+
+int getColorMap()
+{
+    int majorColorIndex;
+    int minorColorIndex;
+	char ColorMapRow[100];
+	int pair=0;
+    for(majorColorIndex=0; majorColorIndex < numberOfMajorColors; majorColorIndex++)
+    {
+        for(minorColorIndex=0; minorColorIndex < numberOfMinorColors; minorColorIndex++)
+        {
+            sprintf(ColorMapRow, "%d\t|%s\t\t|%s\n",(getPairNumber(majorColorIndex,minorColorIndex)),(MajorColorNames[majorColorIndex]),(MinorColorNames[minorColorIndex]));
+            strcpy(&colorMapInfo[(majorColorIndex * numberOfMajorColors + minorColorIndex)][0],ColorMapRow);
+			printf("%s",&colorMapInfo[(majorColorIndex * numberOfMajorColors + minorColorIndex)][0]);
+			pair++;
+        }
+    }
+    return(majorColorIndex*minorColorIndex);
+}
+
+int printColorMap() 
+{
+    int totalPairNumber;
+    printf("Color Map Generated\n\n");
+    totalPairNumber=getColorMap();
+    return (totalPairNumber);
+}
+
+void generateColorCodeReferenceManual()
+{
+    int pairNumber = 1;
+	char ColorMapRow[100];
+	printf("\n Actual Color Code Refernece Manual\n\n");
+    for(int majorColorIndex=0; majorColorIndex < numberOfMajorColors; majorColorIndex++)
+    {
+        for(int minorColorIndex=0; minorColorIndex < numberOfMinorColors; minorColorIndex++)
+        {
+            sprintf(ColorMapRow, "%d\t|%s\t\t|%s\n",pairNumber++,(MajorColorNames[majorColorIndex]),(MinorColorNames[minorColorIndex]));
+			strcpy(&ColorCodeReferenceManual[(majorColorIndex * numberOfMajorColors + minorColorIndex)][0],ColorMapRow);
+            printf("%s",&ColorCodeReferenceManual[(majorColorIndex * numberOfMajorColors + minorColorIndex)][0]);
+        }
+    }
+    
+}
+
+
+int main() 
+{
     int result = printColorMap();
-    assert(result == maxCombination);
-    testColorMap();
+    generateColorCodeReferenceManual();
+    for(int combination_count=0;combination_count<MAX_COLOR_PAIR;combination_count++)
+    {
+       assert(strcmp(&ColorCodeReferenceManual[combination_count][0],colorMapInfo[combination_count][0])==0);
+       
+    }
     printf("All is well (maybe!)\n");
     return 0;
 }
